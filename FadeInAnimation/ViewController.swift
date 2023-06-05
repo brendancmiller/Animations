b6836a1f2a86324383404646011c76461947ae97
Brendan <brendancmiller@gmail.com>
2023-06-04 20:50:18 -0400

    initial commit

--- FadeInAnimation/ViewController.swift
+++ FadeInAnimation/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseIn, animations: {
+            self.image.alpha = 1.0
+        }, completion: nil)
     }
 
 
